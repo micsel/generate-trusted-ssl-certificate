ac9dc699880ce827e62768b3ceac3081329d544a
micsel <micseltene.dev@gmail.com>
2018-11-16 21:44:56 +0000

    pem generate added

--- generate.sh
+++ generate.sh
@@ -1,12 +1,26 @@
 #!bin/bash
 
+if [ ! -d $PWD/certificates ]; then
+  mkdir -p $PWD/certificates;
+fi
 openssl req \
     -newkey rsa:2048 \
     -x509 \
     -nodes \
-    -keyout server.key \
+    -keyout certificates/server.key \
     -new \
-    -out server.crt \
+    -out certificates/server.crt \
+    -config ./openssl-custom.cnf \
+    -sha256 \
+    -days 365
+
+openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout certificates/key.pem \
+    -new \
+    -out certificates/cert.pem \
     -config ./openssl-custom.cnf \
     -sha256 \
     -days 365
